0bd9be702eddefbbcc3543fe6f0f656f6a4e3e27
Luizsoh <souza_luiz@outlook.com.br>
2018-05-19 10:33:17 -0300

    adicionando rotas

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarAlunosComponent } from '../cadastrar-alunos/cadastrar-alunos.component';
+import { ListaPresencaComponent } from '../lista-presenca/lista-presenca.component';
+import { LoginComponent } from '../login/login.component';
+import { MenuComponent } from '../menu/menu.component';
+import { AppComponent } from '../app.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { 
+
+}
